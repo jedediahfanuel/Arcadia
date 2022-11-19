95f94c5e2ccc163aceeabbec7df51fbd91b0e72f
Jeddi212 <jedediahf51@gmail.com>
2022-11-19 17:07:25 +0700

    🌆 :: change grave -> ground

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -72,7 +72,7 @@ public class Jump : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name == "grave")
+        if(collision.gameObject.name == "ground")
         {
             isGrounded = true;
             c = 1;
