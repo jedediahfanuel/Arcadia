ab56e075a6d2b96dc747dde9edfcd38fb2610755
Jeddi212 <jedediahf51@gmail.com>
2022-11-20 01:25:21 +0700

    🔉 ++ sfx jump, fuel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,11 +9,37 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     private int score;
 
+    [SerializeField]
+    private AudioClip fuelSound;
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        CheckAudioSource();
+
+        if (scoreText == null) Debug.LogError("The score text in -game manager- is NULL");
+        
         score = 0;
         UpdateScore(0);
+    }
+
+    private void CheckAudioSource()
+    {
+        if (audioSource == null)
+        {
+            Debug.LogError("The audio source in the -game manager- is NULL | the audio source commponent have not been added before");
+        }
+        else
+        {
+            audioSource.clip = fuelSound;
+        }
+    }
+
+    public void PlayFuelSound()
+    {
+        audioSource.PlayOneShot(fuelSound);
     }
 
     // Update is called once per frame
