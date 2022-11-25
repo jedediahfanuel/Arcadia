f98ca61c25a4cb08b8da0337a950ab26dfda3c60
Jeddi212 <jedediahf51@gmail.com>
2022-11-25 23:06:09 +0700

    ⏸ :: Pause menu integration

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -39,6 +39,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Debug.Log("Loading Menu...");
+        SceneManager.LoadScene(1);
     }
 
     public void RestartGame()
