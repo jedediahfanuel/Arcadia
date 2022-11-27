62bfeb2a0d82e9e1ee119ba60985a41b4482c05c
Jeddi212 <jedediahf51@gmail.com>
2022-11-27 16:18:23 +0700

    🧪 :: Data Persistance Script

---
+++ Assets/Scripts/DataPersistance.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPersistance : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.Save();
+    }
+
+    public void SetHighscoreStage(string stage, int newScore)
+    {
+        switch (stage)
+        {
+            case "D" or "E" or "J" or "T":
+                if (PlayerPrefs.HasKey(stage))
+                {
+                    if (GetHighscore(stage) < newScore)
+                    {
+                        PlayerPrefs.SetInt(stage, newScore);
+                    }
+                }
+                break;
+            default:
+                Debug.LogError("The stage name is invalid | HIGHSCORE");
+                break;
+        }
+    }
+
+    public void SetMoney(int newMoney)
+    {
+        PlayerPrefs.SetInt("M", newMoney);
+    }
+
+    public void SetPowerUp(int newPowerUp)
+    {
+        PlayerPrefs.SetInt("P", newPowerUp);
+    }
+
+    public int GetHighscore(string stage)
+    {
+        return PlayerPrefs.GetInt(stage, 0);
+    }
+
+    public int GetMoney()
+    {
+        return PlayerPrefs.GetInt("M", 0);
+    }
+
+    public int GetPowerUp()
+    {
+        return PlayerPrefs.GetInt("P", 0);
+    }
+}
